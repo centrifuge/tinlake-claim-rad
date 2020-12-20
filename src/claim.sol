02d97aecc45c9e80453529979b21898669f65d95
Jenny Pollack <jennypollack3@gmail.com>
2020-12-20 17:19:39 +0100

    add event for msg.sender

--- src/claim.sol
+++ src/claim.sol
@@ -2,9 +2,11 @@ pragma solidity >=0.5.15;
 
 contract TinlakeClaimRAD {
     mapping (address => bytes32) public accounts;
+    event Claimed(address claimer, bytes32 account);
 
     function update(bytes32 account) public {
         require(account != 0);
         accounts[msg.sender] = account;
+        Claimed(msg.sender, account);
     }
 }
